e2c068ea4981afe2c6e7661239feb0d8da8c1ab6
fairyos <amanre556@gmail.com>
2021-05-13 07:21:04 +0200

    bspwm

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+
+# set wallpaper
+#wal -i "/usr/share/backgrounds/fairyos-wallpaper-v1"
+wal -i "/usr/share/backgrounds/fairyos-wallpapers/beautiful_wal.png"
+#wal -i "$HOME/.config/bspwm/wall.jpg"
+$HOME/.config/polybar/launch_bspwm.sh &
+$HOME/.conky/conky-pywal/./start_conky.sh &
+xsetroot -cursor_name left_ptr &
+#conky -c $HOME/.config/conky/AUR-Nemesis.conkyrc &
+run "$HOME/.config/bspwm//scripts/dualscreen.sh" &
+run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+run plank &
+#run xfce4-panel &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+#blueberry-tray &
+picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#run volumeicon &
+
