6c6a7f9d7193eff6e7d056d66062d3cfeeec7306
fairyos <amanre556@gmail.com>
2021-05-15 02:38:48 +0200

    update

--- AMANRE-Desktops-Configs/0-change-pacman.conf-and-backup-old.sh
+++ AMANRE-Desktops-Configs/0-change-pacman.conf-and-backup-old.sh
@@ -4,7 +4,7 @@
 echo "backup pacman.conf and install new pacman.conf"
 sudo cp /etc/pacman.conf /etc/pacman.conf.backup
 sudo cp -f pacman.conf /etc/pacman.conf
-
+sudo pacman -Sy
 echo
 echo "###############################################################################"
 echo "###                               DONE                                     ####"
