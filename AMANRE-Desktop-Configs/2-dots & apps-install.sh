35bee0bb1cbcbf6ba6ac6c17b64251d5930efaed
fairyos <amanre556@gmail.com>
2021-05-15 03:27:54 +0200

    update

--- AMANRE-Desktop-Configs/2-dots & apps-install.sh
+++ AMANRE-Desktop-Configs/2-dots & apps-install.sh
@@ -40,12 +40,10 @@ PKGS=(
 	'fairyos-dconf-git'
 
 	# THEMEING --------------------------------------------------------------------
-	#'bibata-curser-theme'
-	'bibata-rainbow-curser-theme'
+	'bibata-rainbow-cursor-theme'
 	'candy-gtk-theme'
 	'candy-icon-git'
 	'fairyos-beautyline-git'
-	#'fairyos-fonts-git'
 	'fairyos-gtk-themes-git'
 	'fairyos-icon-themes-git'
 	'fairyos-openbox-themes-git'
@@ -80,9 +78,12 @@ sudo chsh $USER -s /bin/zsh
 git clone --recursive --separate-git-dir=$HOME/.dots.git https://github.com/amanre/dots.git /tmp/dots
 rsync -rvl --exclude ".git" /tmp/dots/ $HOME/
 rm -r /tmp/dots
-rm -r /~/.dots.git
 echo
 echo "INSTALLATION COMPLETE"
 echo
 sleep 2
+echo
+echo "WE WILL REBOOT NOW"
+echo
+sleep 3
 reboot
