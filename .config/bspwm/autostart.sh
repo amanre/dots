acb1947f0884d7bbf85e0831c2df498f7129bf59
amanre <amanre556@gmail.com>
2021-07-10 18:19:06 +0200

    update

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -10,11 +10,11 @@ function run {
 
 # set wallpaper
 #wal -i "/usr/share/backgrounds/fairyos-wallpaper-v1"
-wal -i /usr/share/endeavouros/backgrounds/eos_wallpapers_community/blinky_1.png 
+#wal -i /usr/share/backgrounds/amos-wallpapers/beautiful_wal.jpg
 #wal -i "$HOME/.config/bspwm/wall.jpg"
-bash ~/.config/polybar/blocks/scripts/pywal.sh /usr/share/endeavouros/backgrounds/eos_wallpapers_community/krimkerre_2_poly.jpg
-bash ~/.config/polybar/launch.sh --blocks
-#$HOME/.config/polybar/launch_bspwm.sh &
+#bash ~/.config/polybar/blocks/scripts/pywal.sh /usr/share/endeavouros/backgrounds/eos_wallpapers_community/krimkerre_2_poly.jpg
+#bash ~/.config/polybar/launch.sh --blocks
+$HOME/.config/polybar/launch_bspwm.sh &
 $HOME/.conky/conky-pywal/./start_conky.sh &
 xsetroot -cursor_name left_ptr &
 #conky -c $HOME/.config/conky/AUR-Nemesis.conkyrc &
