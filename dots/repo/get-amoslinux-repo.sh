c47027edbd24d410a1af2d79789a2ab40c2428f3
amanre <amanre556@gmail.com>
2024-10-10 02:16:46 +0200

    update

--- dots/repo/get-amoslinux-repo.sh
+++ dots/repo/get-amoslinux-repo.sh
@@ -3,7 +3,13 @@ echo '
 
 [amoslinux_repo]
 SigLevel = Optional TrustedOnly
-Server = https://amanre.github.io/$repo/$arch' | sudo tee --append /etc/pacman.conf
+Server = https://amanre.github.io/$repo/$arch
+
+[gh0stzk-dotfiles]
+SigLevel = Optional TrustAll
+Server = http://gh0stzk.github.io/pkgs/x86_64' | sudo tee --append /etc/pacman.conf
+
+
 
 
 
