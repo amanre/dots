82f1bea2c085fb0a4ff7c3e21acd9c1539eea984
amanre <amanre556@gmail.com>
2024-11-13 03:57:56 +0100

    update

--- dots/Personal/skel.sh
+++ dots/Personal/skel.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 #set -e
-##################################################################################################################
-# Author 	: Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website   : https://www.alci.online
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
+
 ##################################################################################################################
 #tput setaf 0 = black
 #tput setaf 1 = red
@@ -26,7 +13,7 @@
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-# when on EOS
+
 
 ###############################################################################
 #
@@ -40,9 +27,9 @@ echo "FINAL SKEL"
 echo "Copying all files and folders from /etc/skel to ~"
 echo "################################################################"
 echo;tput sgr0
-sudo pacman -S archlinux-appstream-data --noconfirm
+#sudo pacman -S archlinux-appstream-data --noconfirm
 #cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S)
-cp  /etc/skel/. ~
+cp   -arf /etc/skel /. ~
 
 
 tput setaf 2
